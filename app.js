75cb43e78fe5987062903a90d8a62dd8171ead3b
masatheultimater <mysnowsmile@gmail.com>
2021-05-05 18:20:13 +0000

    ADD route to / & static folder

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen('3000', () => {
   console.log('server is running on port 3000');
 });
