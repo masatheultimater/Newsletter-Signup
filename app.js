22542172ec0908b908f84fb5ce0d2152445ce8c7
masatheultimater <mysnowsmile@gmail.com>
2021-05-05 18:38:36 +0000

    ADD post routing

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const request = require('request');
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen('3000', () => {
